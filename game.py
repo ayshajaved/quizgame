58e7a2a1911e24ada51526d7cf24f76fddb27f64
ayshajaved <ayshajavd@gmail.com>
2024-07-01 12:01:00 +0500

    game
    This is the basic one question game.
    Will be moving from simple to complex

---
+++ game.py
@@ -0,0 +1,8 @@
+print("1)What is the most popoular and easy computer language?")
+print("A:C\nB:Pyhton\nC:C++\nD:Java")
+answer = input("Enter your answer: ") #you can write in upper or lower
+if(answer == "A" or "a"):
+    print("Correct Answer!")
+else:
+    print("Wrong Answer!")
+
